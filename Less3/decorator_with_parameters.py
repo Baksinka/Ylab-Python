680e3ed046b8692920ab42e55e7d688d3bd9eddd
Baksinka <nats4@mail.ru>
2022-07-14 15:42:46 +0300

    Внесены изменения в соответствии с замечаниями

--- Less3/decorator_with_parameters.py
+++ Less3/decorator_with_parameters.py
@@ -6,25 +6,31 @@ def extern_dec_param(call_count, start_sleep_time, factor, border_sleep_time):
     def dec_param(funс):
 
         def wrapper(*args, **kwargs):
+            print('Начало работы')
             number = args[0]
             func_result = funс(*args, **kwargs)
             times = start_sleep_time
             counter = 0
             while times < border_sleep_time:
                 time.sleep(times)
-                times *= factor
                 func_result = funс(*args, **kwargs)
+                print(f'Запуск номер {counter + 1}. Ожидание: {times} секунд. Результат декорируемой функции = '
+                      f'{func_result}')
+                times *= factor
                 counter += 1
             if counter < call_count:
                 for i in range(call_count - counter):
                     time.sleep(border_sleep_time)
                     func_result = funс(*args, **kwargs)
+                    print(f'Запуск номер {i + 1 + counter}. Ожидание: {border_sleep_time} секунд. Результат '
+                          f'декорируемой функции = {func_result}')
+            print('Конец работы')
             return func_result
         return wrapper
     return dec_param
 
 
-@external_dec_param(7, 1, 2, 10)
+@extern_dec_param(7, 1, 2, 10)
 def multiplier(number: int):
     return number * 2
 
--- Less3/solid_code/alert.py
+++ Less3/solid_code/alert.py
@@ -1,13 +1,19 @@
-class Alert:
+from abc import ABC, abstractmethod
+
+
+class Alert(ABC):
+
+    @abstractmethod
+    def create_news(self, place, hero):
+        ...
+
+
+class TV(Alert):
 
     def create_news(self, place, hero):
         place_name = getattr(place, 'name', 'place')
         hero_name = getattr(hero, 'name', 'hero')
         print(f'{hero_name} saved the {place_name}!')
-
-
-class TV(Alert):
-    ...
 
 
 class NewsPlanet(Alert):
